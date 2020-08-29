71ae75daab615c2e1c7303c7b689c2efd10f0800
Alireza Choobineh <59641878+W1ckedD@users.noreply.github.com>
2020-08-29 22:16:33 +0430

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { createAdmin, signin } = require('../controllers/admin');
 
-router.post('/create-admin', createAdmin);
+// router.post('/create-admin', createAdmin);
 router.post('/signin', signin);
 
 module.exports = router;
